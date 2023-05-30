202a96f7515c8585ff5164aaa18d92f4a9d12379
sirilaktem <sirilaktem@outlook.com>
2023-05-30 10:58:14 -0700

    Create new branch for API Call Challenge

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,19 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
 
-    Client.checkForName(formText)
+    Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+    console.log('::: Form Submitted :::');
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=541edf4d85a8f53fc2d7080a41292ff2&units=imperial&zip=91748')
+        .then((res) => {
+            return res.json();
+        })
+        .then(function (data) {
+            document.getElementById('results').innerHTML = data.main.temp;
+        });
 }
 
-export { handleSubmit }
+export { handleSubmit };
